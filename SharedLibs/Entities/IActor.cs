f914e7b314f2ce7e935945c98cf0a497e911bf58
Justin Wright <jwright@lineargs.com>
2019-09-12 12:43:45 -0400

    Add project files.

---
+++ SharedLibs/Entities/IActor.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FuzeRPG.Common.Entities
+{
+    internal interface IActor
+    {
+        double Health { get; set; }
+        double Stamina { get; set; }
+        double Mana { get; set; }
+
+        IContainer Inventory { get; }
+
+        void Equip(IEquipment equipment);
+
+        void Unequip(IEquipment equipment);
+
+        void Fight(IActor target);
+
+        void Consume(IConsumable consumable);
+    }
+}
